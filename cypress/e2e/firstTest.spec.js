3aa88edabde64b03f84469687693353c99bf2dc2
e-nie <eniehaus111@gmail.com>
2024-07-17 17:39:59 +0200

    saving subject of the command

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -39,7 +39,7 @@ describe('First test suite', () => {
 
     })
 
-    it.only('Second test case', () => {
+    it('Second test case', () => {
         cy.visit('/')
         cy.contains('Forms').click()
         cy.contains('Form Layouts').click()
@@ -65,12 +65,35 @@ describe('First test suite', () => {
             .find('nb-checkbox')
             .click()
 
+    })
+    it.only('Save subject of the command', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
 
 
 
+        // cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'Email')
+        // cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain', 'Password')
+
+        //here we are duplicating the code, we can not save the repeating code in a variable: INCORRECT APPROACH:
+        // const usingTheGrid = cy.contains('nb-card', 'Using the Grid')
+        // usingTheGrid.find('[for="inputEmail1"]').should('contain', 'Email')//сработает
+        // usingTheGrid.find('[for="inputPassword2"]').should('contain', 'Password') //а два не сработает уже, тк
+
+        // ❗️Saving locators - CORRECT APPROACH:
+
+        //1. Cypress alias
+        cy.contains('nb-card', 'Using the Grid').as('usingTheGrid')
+        cy.get('@usingTheGrid').find('[for="inputEmail1"]').should('contain', 'Email')
+        cy.get('@usingTheGrid').find('[for="inputPassword2"]').should('contain', 'Password')
 
 
+        //2. Using the cypress then() method
 
-
+        cy.contains('nb-card', 'Using the Grid').then(usingTheGridForm => {
+            cy.wrap(usingTheGridForm).find('[for="inputEmail1"]').should('contain', 'Email')
+            cy.wrap(usingTheGridForm).find('[for="inputPassword2"]').should('contain', 'Password')
+        })
     })
 })
