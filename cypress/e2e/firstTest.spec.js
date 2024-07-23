5619c27714fd7d9144f56774ef3fd85eeeee70a7
e-nie <eniehaus111@gmail.com>
2024-07-23 22:32:05 +0200

    add lists and dropdowns test

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -167,7 +167,7 @@ describe('First test suite', () => {
         cy.get('[type="checkbox"]').check({ force: true })
     })
 
-    it.only('Datepicker', () => {
+    it('Datepicker', () => {
 
         function selectDay(day) {
             let date = new Date()
@@ -196,5 +196,31 @@ describe('First test suite', () => {
             cy.wrap(input).invoke('prop', 'value').should('contain', dateToAssert)
             cy.wrap(input).should('have.value', dateToAssert)
         })
+    })
+
+    it.only('Lists and dropdowns', () => {
+        cy.visit('/')
+
+        //1.
+        cy.get('nav nb-select').click()
+        cy.get('.options-list').contains('Dark').click()
+        cy.get('nav nb-select').should('contain', 'Dark')
+
+        //2. Looping through the dropdown list to select all the options one by one:
+
+        cy.get('nav nb-select').then(dropdown => {
+            cy.wrap(dropdown).click()
+            cy.get('.options-list nb-option').each((listItem, index) => {
+                const itemText = listItem.text().trim()
+                cy.wrap(listItem).click()
+                cy.wrap(dropdown).should('contain', itemText)
+                if (index < 3) {
+                    cy.wrap(dropdown).click()
+                }
+
+            })
+
+        })
+
     })
 })
