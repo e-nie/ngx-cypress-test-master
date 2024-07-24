ebca9f0f53e3c3162bd903a2b3b8352e0f90abd6
e-nie <eniehaus111@gmail.com>
2024-07-24 23:39:52 +0200

    add web tables test 1

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -198,7 +198,7 @@ describe('First test suite', () => {
         })
     })
 
-    it.only('Lists and dropdowns', () => {
+    it('Lists and dropdowns', () => {
         cy.visit('/')
 
         //1.
@@ -223,4 +223,33 @@ describe('First test suite', () => {
         })
 
     })
+
+    it.only('Web tables', () => {
+        cy.visit('/')
+        cy.contains('Tables & Data').click()
+        cy.contains('Smart Table').click()
+
+        //1. Get the row of the table by the text of the cell:
+        cy.get('tbody').contains('tr', 'Larry').then(tableRow => {
+            cy.wrap(tableRow).find('.nb-edit').click()
+            cy.wrap(tableRow).find('[placeholder="Age"]').clear().type('25')
+            cy.wrap(tableRow).find('.nb-checkmark').click()
+            cy.wrap(tableRow).find('td').eq(6).should('contain', '25')
+        })
+
+        //2. Get the row by index:
+        cy.get('thead').find('.nb-plus').click()
+        cy.get('thead').find('tr').eq(2).then(tableRow => {
+            cy.wrap(tableRow).find('[placeholder="First Name"]').type('Adam')
+            cy.wrap(tableRow).find('[placeholder="Last Name"]').type('Sandler')
+            cy.wrap(tableRow).find('.nb-checkmark').click()
+            cy.get('tbody tr').first().find('td').then(tableColumns => {
+                cy.wrap(tableColumns).eq(2).should('contain', 'Adam')
+                cy.wrap(tableColumns).eq(3).should('contain', 'Sandler')
+            })
+        })
+
+
+    })
+
 })
