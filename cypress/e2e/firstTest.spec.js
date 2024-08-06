5d2f4c80a71c0fd5a46fdcfeead90f9028a2f37c
e-nie <eniehaus111@gmail.com>
2024-08-07 00:27:33 +0200

    add popups and tooltips

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -224,7 +224,7 @@ describe('First test suite', () => {
 
     })
 
-    it.only('Web tables', () => {
+    it('Web tables', () => {
         cy.visit('/')
         cy.contains('Tables & Data').click()
         cy.contains('Smart Table').click()
@@ -269,6 +269,44 @@ describe('First test suite', () => {
 
     })
 
+    it('tooltip', () => {
+        cy.visit('/')
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Tooltip').click()
+        cy.contains('nb-card', 'Colored Tooltips').contains('Default').click()
+        cy.get('nb-tooltip').should('contain', 'This is a tooltip')
 
+    })
+
+    it('dialog box', () => {
+        cy.visit('/')
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Dialog').click()
+        cy.contains('nb-card', 'Open Dialog').contains('Open Dialog with component').click()
+        cy.get('nb-card-header').should('contain', 'This is a title passed to the dialog component')
+    })
+
+    //interacting with alerts and dialog boxes
+    it.only('alert/dialog box', () => {
+        cy.visit('/')
+        cy.contains('Tables & Data').click()
+        cy.contains('Smart Table').click()
+
+        //1
+        // cy.get('tbody tr').first().find('.nb-trash').click()
+        // cy.on('window:confirm', (confirm) => {
+        //     expect(confirm).to.equal('Are you sure you want to delete?')
+        // })
+
+        //2
+        // const stub = cy.stub()
+        // cy.on('window:confirm', stub)
+        // cy.get('tbody tr').first().find('.nb-trash').click().then(() => {
+        //     expect(stub.getCall(0)).to.be.calledWith('Are you sure you want to delete?')
+        // })
+
+        //3
+        cy.get('tbody tr').first().find('.nb-trash').click()
+        cy.on('window:confirm', (confirm) => false)
+    })
 })
-
